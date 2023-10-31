65fe066b80dc78744e80a9dc206c794360e25a5e
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-10-31 23:55:07 +0530

    if else

---
+++ d.cpp
@@ -0,0 +1,17 @@
+//if else 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int money;
+    cin>>money;
+
+    if(money>5000){
+        cout<<"5 Star Hotel"<<endl;
+    }
+    else{
+        cout<<"Home"<<endl;
+    }
+    return 0;
+}
