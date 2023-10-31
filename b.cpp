f90bf97a569d5723dc00130a5ef2073fc2a974d2
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-10-31 23:53:48 +0530

    sizeof program

---
+++ b.cpp
@@ -0,0 +1,18 @@
+//program of sizeof()
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"size of int"<<sizeof(n)<<endl;
+
+    float m;
+    cout<<"size of int"<<sizeof(m)<<endl;
+
+    char o;
+    cout<<"size of int"<<sizeof(o)<<endl;
+
+
+    return 0;
+}
