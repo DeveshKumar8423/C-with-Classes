909b0c4ce0131aa603a09d7f2f187f73d38d06fd
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-10-31 23:55:49 +0530

    even odd program

---
+++ f.cpp
@@ -0,0 +1,17 @@
+//even odd program
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    if(a % 2 == 0){
+        cout<<"This is an Even number"<<endl;
+    }
+    else{
+        cout<<"This is an Odd number"<<endl;
+    }
+    return 0;
+}
