e2435d2be86efad5587201242c5e42a20529aa44
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-01 23:50:57 +0530

    for loop

---
+++ g.cpp
@@ -0,0 +1,16 @@
+//for loop for sum 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    int sum = 0;
+    cin>>a;
+
+    for(int i = 0 ; i<=a ; i++){
+        sum = sum + i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
