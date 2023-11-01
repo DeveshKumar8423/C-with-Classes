2be2c308d760e6224af918b7d72bb998bc47fef6
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-01 23:52:48 +0530

    prime number

---
+++ l.cpp
@@ -0,0 +1,21 @@
+//break and continue 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int i;
+
+    for( i=2 ; i<n ; i++){
+        if(n%i==0){
+            cout<<"Not a prime number"<<endl;
+            break;
+        }
+    }
+    if(i==n){
+        cout<<"Prime Number"<<endl;
+    }
+    return 0;
+}
