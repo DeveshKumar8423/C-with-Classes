34934ba68f0d13f9533719b8b267d31be3d1d31a
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-01 23:51:08 +0530

    while loop

---
+++ h.cpp
@@ -0,0 +1,15 @@
+//while loop program
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    while(a>0){
+        cout<<a<<endl;
+        cin>>a;
+    }
+    return 0;
+}
