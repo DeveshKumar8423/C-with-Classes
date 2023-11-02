1ea177d7a470f12030f45259989cd119d3e2bb90
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-02 20:09:34 +0530

    inverted pyramid pattern

---
+++ q.cpp
@@ -0,0 +1,18 @@
+//inverted pyramid pattern
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter value"<<endl;
+    cin>>n;
+
+    for(int i = n ; i>=1 ; i--){
+        for(int j = 1 ; j<=i ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
