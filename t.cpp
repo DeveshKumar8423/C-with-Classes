b5be559f4792120a1dc14ca4f3a2b3f0183c76b8
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-02 20:10:23 +0530

    Floyds triangle pattern

---
+++ t.cpp
@@ -0,0 +1,20 @@
+//Floyds triangle pattern
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter value"<<endl;
+    cin>>n;
+    int count = 1;
+
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = 1 ; j<=i ; j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
