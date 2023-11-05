55e9b29b0368555f3fdebfe18d5b6159d869193d
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-05 12:13:07 +0530

    check given number is pythagorean triplet or not

---
+++ al.cpp
@@ -0,0 +1,45 @@
+//check given number is pythagorean triplet or not
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int check(int x , int y , int z){
+    int a = max(x  , max(y,z));
+    int b ,c;
+
+    if(a == x){
+        b = y;
+        c = z;
+    }
+    else if(a==y){
+        b = x;
+        c = z;
+    }
+    else{
+        b=x;
+        c=y;
+    }
+
+    if(a*a == b*b + c*c){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+    }
+
+
+int main(){
+    int x ,y ,z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,z)){
+        cout<<"Pythagorean triplet";
+    }
+    else{
+        cout<<"Not a Pythagorean triplet";
+    }
+
+    return 0;
+}
