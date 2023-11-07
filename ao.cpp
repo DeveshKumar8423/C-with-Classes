c397d099740d10e572174a2b7bce3d0961aea4ad
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-11-07 23:39:30 +0530

    hexadecimal to decimal

---
+++ ao.cpp
@@ -0,0 +1,28 @@
+//hexadecimal to decimal
+#include<bits/stdc++.h> //it includes all the header files like string , vectors etc
+using namespace std;
+
+int hexadecimaltodecimal(string n){
+    int ans = 0;
+    int x = 1;
+    
+    int s = n.size();
+    for(int i=s-1 ; i>=0 ; i--){
+        if(n[i] >= '0' && n[i] <= '9'){
+            ans+=x*(n[i]-'0');
+        }
+        else if(n[i] >= 'A' && n[i] <= 'F'){
+            ans+=x*(n[i]-'A'+10);
+        }
+        x*=16;
+    }
+    return ans;
+    }
+
+int main(){
+    string n;
+    cin>>n;
+    
+    cout<<hexadecimaltodecimal(n)<<endl;
+    return 0;
+}
